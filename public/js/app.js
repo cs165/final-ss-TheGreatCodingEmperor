5e3b307ad4dbd8a0eaa03f24dbcce79a981028c5
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2019-06-22 12:15:27 +0800

    finish basic

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class App{
     constructor(id){
         const main = document.querySelector('.main');
@@ -8,7 +7,7 @@ class App{
         const body = document.querySelector('body');
 
         const container = document.createElement('div');
-        container.classList.add('.main');
+        container.classList.add('main');
         body.appendChild(container);
 
         const diary = document.createElement('div');
@@ -20,52 +19,23 @@ class App{
         textarea.classList.add('text');
         container.appendChild(textarea);
 
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('buttonContainer');
+        container.appendChild(buttonContainer);
+
         const yesterday = document.createElement('button');
         yesterday.classList.add('yesterday');
         yesterday.textContent="yesterday";
-        container.appendChild(yesterday);
+        buttonContainer.appendChild(yesterday);
 
         const home = document.createElement('button');
         home.classList.add('home');
         home.textContent="update";
-        container.appendChild(home);
+        buttonContainer.appendChild(home);
 
         const tomorrow = document.createElement('button');
         tomorrow.classList.add('tomorrow');
         tomorrow.textContent="tomorrow";
-        container.appendChild(tomorrow);
+        buttonContainer.appendChild(tomorrow);
     }
-=======
-class App{
-    constructor(id){
-        const main = document.querySelector('.main');
-        main.classList.add('hide');
-
-        //window.location.href = "localhost:3000/id/"+id;
-        const body = document.querySelector('body');
-
-        const container = document.createElement('div');
-        container.classList.add('.main');
-        body.appendChild(container);
-
-        const diary = document.createElement('div');
-        diary.classList.add('title');
-        diary.textContent=id;
-        container.appendChild(diary);
-
-        const textarea = document.createElement('textarea');
-        textarea.classList.add('text');
-        container.appendChild(textarea);
-
-        const yesterday = document.createElement('button');
-        yesterday.classList.add('yesterday');
-        yesterday.textContent="yesterday";
-        container.appendChild(yesterday);
-
-        const tomorrow = document.createElement('button');
-        tomorrow.classList.add('tomorrow');
-        tomorrow.textContent="tomorrow";
-        container.appendChild(tomorrow);
-    }
->>>>>>> 3baf76fd1c84acdc764b1d64c68043d23dc8b1d3
 }
